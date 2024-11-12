8da52ba5987169921235e27aa8d389126782f79a
Aman kr <princraj0088@gmail.com>
2024-11-12 09:10:00 +0530

    Changing the directory name

--- src/nov30/digitsum/TwoDigitSumMain.java
+++
@@ -1,9 +0,0 @@
-package nov30.digitsum;
-
-public class TwoDigitSumMain {
-
-	public static void main(String[] args) {
-		int num = 34;
-		System.out.println("Digit sum of " + num + " is : " + TwoDigitSum.getSumOfDigits(num));
-	}
-}
--- src/nov30/multiple/NextMultipleOfHundred.java
+++
@@ -1,9 +0,0 @@
-//Define a method which returns the next multiple of 100 for the given number.Write the method with the following specifications:Name of the BLC class:NextMultipleOfHundred Name of method:getNextMultipleOfHundred()Access Modifier:public,static Arguments:one argument of type integer Return type:an integer value For example,If the given value is 123,return 200.
-
-package nov30.multiple;
-
-public class NextMultipleOfHundred {
-	public static int getNextMultipleOfHundred(int num) {
-		return (num / 100 + 1) * 100;
-	}
-}
